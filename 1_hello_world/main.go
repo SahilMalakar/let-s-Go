3cae5e55ab22dfc8d8c294efe6673bd96a759815
Sahil Malakar <sahilmalakar150@gmail.com>
2026-02-21 22:12:09 +0300

    _helloWorld

---
+++ 1_hello_world/main.go
@@ -0,0 +1,7 @@
+package main        // Defines the main package (entry point of the program)
+
+import "fmt"        // Imports fmt package for input/output functions
+
+func main() {       // main function where program execution starts
+    fmt.Println("hello world") // Prints text to the console
+}
